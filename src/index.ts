2ec0a5aa3adc27b3e000536e270f43aa8aa7145e
Michael Bock <me@thought2.de>
2021-06-18 07:12:08 +0200

    Add `tag` implementation

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,102 @@
 import { pipe } from 'fp-ts/function'
 
 // -----------------------------------------------------------------------------
-// greetings
+// Internal
+// -----------------------------------------------------------------------------
+
+const defaultConfig = {
+  tag: '_tag' as const,
+}
+
+type IsData = Record<string, unknown>
+
+type IsTag = string
+
+type DefaultConfig = { tag: '_tag' }
+
+// -----------------------------------------------------------------------------
+// Types
 // -----------------------------------------------------------------------------
 
 /**
- * It's a greeting
- *
  * @since 1.0.0
- * @category Greetings
- * @example
- *   import { greet } from 'fp-ts-tagged-structs'
- *   assert.deepStrictEqual(greet('World'), 'Hello, World!')
+ * @category Types
  */
-export const greet = (name: string): string =>
-  pipe(`Hello`, (x) => `${x}, ${name}!`)
+export type IsConfig = {
+  tag: string
+}
+
+/**
+ * @since 1.0.0
+ * @category Types
+ */
+export type Tag<
+  T extends IsTag,
+  D extends IsData,
+  Cfg extends IsConfig = DefaultConfig
+> = {
+  [key in Cfg['tag']]: T
+} &
+  D
+
+/**
+ * @since 1.0.0
+ * @category Types
+ */
+export type UnTag<
+  D extends IsData,
+  Cfg extends IsConfig = DefaultConfig
+> = Omit<D, Cfg['tag']>
+
+// -----------------------------------------------------------------------------
+// Non Pipeables
+// -----------------------------------------------------------------------------
+
+/**
+ * @since 1.0.0
+ * @category Non Pipeables
+ */
+export const tagWithConfig_: <
+  T extends IsTag,
+  D extends IsData,
+  Cfg extends IsConfig
+>(
+  tag: T,
+  data: D,
+  config: Cfg
+) => Tag<T, D, Cfg> = (tag, data, cfg) => ({
+  [cfg.tag]: tag,
+  ...data,
+})
+
+/**
+ * @since 1.0.0
+ * @category Non Pipeables
+ */
+export const tag_: <T extends IsTag, D extends IsData>(
+  tag: T,
+  data: D
+) => Tag<T, D> = (t, d) => tagWithConfig_(t, d, defaultConfig)
+
+// -----------------------------------------------------------------------------
+// Pipeables
+// -----------------------------------------------------------------------------
+
+/**
+ * @since 1.0.0
+ * @category Pipeables
+ */
+export const tagWithConfig: <Cfg extends IsConfig>(
+  cfg: Cfg
+) => <T extends IsTag>(
+  tag: T
+) => <D extends IsData>(data: D) => Tag<T, D, Cfg> = (cfg) => (t) => (d) =>
+  tagWithConfig_(t, d, cfg)
+
+/**
+ * @since 1.0.0
+ * @category Pipeables
+ */
+export const tag: <T extends IsTag>(
+  tag: T
+) => <D extends IsData>(data: D) => Tag<T, D> = (t) => (d) => tag_(t, d)
